82cb4f2b4bc560a3cae5f4ae6443da4c944023d5
Dr. Pierre Jourlin <pierre.jourlin@univ-avignon.fr>
2017-02-02 10:53:48 +0100

    Update process_clef.sh

--- applications/clef/process_clef.sh
+++ applications/clef/process_clef.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LANGUAGES="en sv ceb de nl fr ru it es war pl vi ja pt zh uk ca fa no ar sh fi hu id ro cs ko sr ms tr eu eo bg min da kk sk hy zh-min-nan he lt hr ce sl et gl nn uz la el be simple vo hi th az ur ka af  als  am  an  arz  ast  azb  ba  bar  bat-smg  be-tarask  bn  bpy  br  bs  bug  ckb  cv  cy  fo  fy  ga  gd  gu  hsb  ht  ia  io  is  jv  kn  ku  ky  lb  li  lmo  lv  map-bms  mg  mk  ml  mn  mr  mrj  my  mzn  nah  nap  nds  ne  new  oc  or  os  pa  pms  pnb  qu  sa  sah  scn  sco  si  sq  su  sw  ta  te  tg  tl  tt  vec  wa  yi  yo  zh-yue hif  mhr  ilo  roa-tara  pam  xmf  eml  sd  ps  nso  bh  se  hak  mi  bcl  diq  nds-nl  gan  glk  vls  rue  bo  wuu  mai  fiu-vro  co  tk  szl  sc  csb  vep  lrc  km  crh  gv  kv  frr  zh-classical  as  so  zea  cdo  lad  ay  stq  udm  kw  nrm  ie  koi  ace  rm  pcd  myv  lij  mt  fur  gn  dsb  dv  cbk-zam  ext  lez  ug  kab  ang  ksh  mwl  ln  gag  sn  frp  gom  pag  pi  nv  av  pfl  xal  krc  haw  kaa  bxr  rw  pdc  to  nov  kl  pap  lo  arc  bjn  olo  kbd  jam  ha  tet  tyv  ki  tpi  ig  na  lbe  roa-rup  jbo  ty  kg  za  mdf  lg  wo  srn ab  ltg  chr  zu  sm  om  bi  tcy  tn  chy  cu  rmy  tw  xh  tum  pih  rn  got  pnt  ss  ch  bm  ady  mo  ts  ny  iu  st  fj  ee  ak  ks  ik  sg  ve  dz  ff  ti  cr"
-# Trouble with term "cest" in cebuano (ceb) language. "cest" occurs 244012 times !!!
+
 cat data/clef_microblogs_festival.txt | cut -f1,4 |sed 's:^[^0-9].*:xxxxxxxxxxxxxxxxxx\t00:'> data/id_lang.csv
 psql -dclef -c"DROP TABLE microblog ;"
 psql -dclef -c"CREATE TABLE microblog (id serial, id_original varchar(32), lang varchar(50));"
@@ -35,9 +35,9 @@ do
 #    psql -dclef -c "UPDATE dictionaries SET id="$nb"1, lang='"$l"' WHERE id IS NULL AND lang IS NULL;"
     nb=$(($nb+1))
 done
-# Problem with term "cest" (i.e. Central European Summer Time, in cebuano (ceb) language. 
-# "cest" occurs 244012 times in the ceb dictionary !!!
-DELETE FROM dictionaries WHERE lang='ceb' AND term='cest'
+# Problem with term "cest" (i.e. Central European Summer Time, in many languages. 
+# i.e. "cest" occurs 244012 times in the ceb dictionary !!!
+psql -dclef -c "DELETE FROM dictionaries WHERE term='cest';"
 
 #psql -dclef -c "DROP TABLE counting;"
 #psql -dclef -c "CREATE TABLE counting (tweet_id BIGINT, lang CHARACTER VARYING(15), number BIGINT);"
