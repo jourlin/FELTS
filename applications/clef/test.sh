2751927922aa994f3f9b8acaa1bdefc89b64aed2
Dr. Pierre Jourlin <pierre.jourlin@univ-avignon.fr>
2017-05-05 12:16:24 +0200

    Update test.sh

--- applications/clef/test.sh
+++ applications/clef/test.sh
@@ -4,26 +4,34 @@ test_from=100001;
 test_to=200000;
 
 # term frequencies in database :
-CREATE table frequencies AS SELECT term, count(*) as freq FROM term WHERE tweet_id >=$learn_from AND tweet_id<=$learn_to GROUP BY term ORDER BY count(*) DESC;
+psql -c "DROP TABLE IF EXISTS frequencies_mblog; CREATE table frequencies_mblog AS SELECT term, lang, count(*) as freq FROM term, microblog WHERE term.tweet_id=microblog.id GROUP BY term,lang ORDER BY count(*) DESC;"
 # probability of language given a term :
-CREATE INDEX term_idx ON dictionaries USING hash (term);
-DROP TABLE IF EXISTS probabilities_mblog ;CREATE TABLE probabilities_mblog AS SELECT x.term, lang, CAST(count(*) AS float)/(SELECT freq FROM frequencies WHERE frequencies.term=x.term) AS probability FROM (select term, lang from term, microblog WHERE tweet_id=id) as x GROUP BY term, lang ORDER BY probability DESC;
-DROP TABLE IF EXISTS probabilities_wiki ;CREATE TABLE probabilities_wiki AS SELECT x.term, lang, CAST(count(*) AS float)/(SELECT sum(freq) FROM dictionaries WHERE dictionaries.term=x.term) AS probability FROM (select term, lang from dictionaries) as x GROUP BY term, lang ORDER BY probability DESC;
- 
-psql -c "DROP TABLE IF EXISTS counting_mblog;CREATE TABLE counting_mblog (tweet_id BIGINT, lang CHARACTER VARYING(15), probsum FLOAT);"
+psql -c "CREATE INDEX term_idx ON dictionaries USING hash (term);"
+
+# Clean experiments
+psql -c "DROP TABLE IF EXISTS probabilities_mblog ;DROP TABLE IF EXISTS probabilities_wiki ;DROP TABLE IF EXISTS counting_mblog;DROP TABLE IF EXISTS counting_wiki; DROP TABLE IF EXISTS auto_lang_mblog;DROP TABLE IF EXISTS auto_lang_wiki;DROP TABLE IF EXISTS freq_sum_wiki"
+# Calculate word-level probabilities
+psql -c "CREATE TABLE freq_sum_mblog AS SELECT term, sum(freq) FROM frequencies GROUP BY term;"
+psql -c "CREATE TABLE probabilities_mblog AS SELECT x.term, x.lang, (CAST(freq AS FLOAT) / sum) AS probability FROM frequencies_mblog as x, freq_sum_mblog as f WHERE f.term=x.term  GROUP BY x.term, x.lang, x.freq, f.sum ORDER BY probability DESC;"
+#psql -c "CREATE TABLE probabilities_mblog AS SELECT x.term, lang, CAST(count(*) AS float)/(SELECT freq FROM frequencies WHERE frequencies.term=x.term) AS probability FROM (select term, lang from term, microblog WHERE tweet_id=id) as x GROUP BY term, lang ORDER BY probability DESC;"
+psql -c "CREATE TABLE freq_sum_wiki AS SELECT term, sum(freq) FROM dictionaries2 GROUP BY term;"
+psql -c "CREATE TABLE probabilities_wiki AS SELECT x.term, x.lang, (CAST(freq AS FLOAT) / sum) AS probability FROM dictionaries2 as x, freq_sum_wiki as f WHERE f.term=x.term  GROUP BY x.term, x.lang, x.freq, f.sum ORDER BY probability DESC;"
+# calculate message-level probabilities
+psql -c "CREATE TABLE counting_mblog (tweet_id BIGINT, lang CHARACTER VARYING(15), probsum FLOAT);"
 psql -c "INSERT INTO counting_mblog (tweet_id, lang, probsum) SELECT tweet_id, probabilities_mblog.lang, sum(probability) FROM term, probabilities_mblog, task1, microblog WHERE task1.id_original=microblog.id_original AND microblog.id=term.tweet_id AND term.term=probabilities_mblog.term GROUP BY tweet_id, probabilities_mblog.lang;"
-psql -c "DROP TABLE IF EXISTS counting_wiki;CREATE TABLE counting_wiki (tweet_id BIGINT, lang CHARACTER VARYING(15), probsum FLOAT);"
+psql -c "CREATE TABLE counting_wiki (tweet_id BIGINT, lang CHARACTER VARYING(15), probsum FLOAT);"
 psql -c "INSERT INTO counting_wiki (tweet_id, lang, probsum) SELECT tweet_id, probabilities_wiki.lang, sum(probability) FROM term, probabilities_wiki, task1, microblog WHERE task1.id_original=microblog.id_original AND microblog.id=term.tweet_id AND term.term=probabilities_wiki.term GROUP BY tweet_id, probabilities_wiki.lang;"
 
 
 # Automatically choose a language (the one where words are the most frequent) for 100,000 tweets : 
-psql -c "DROP TABLE IF EXISTS auto_lang_mblog;CREATE TABLE auto_lang_mblog AS SELECT DISTINCT ON (tweet_id) tweet_id,counting_mblog.lang, probsum as scoreMB from counting_mblog, task1, microblog WHERE task1.id_original=microblog.id_original AND microblog.id=counting_mblog.tweet_id ORDER BY tweet_id ASC, probsum DESC ;"
-psql -c "DROP TABLE IF EXISTS auto_lang_wiki;CREATE TABLE auto_lang_wiki AS SELECT DISTINCT ON (tweet_id) tweet_id,counting_wiki.lang,probsum as scorewiki from counting_wiki, task1, microblog WHERE task1.id_original=microblog.id_original AND microblog.id=counting_wiki.tweet_id ORDER BY tweet_id ASC, probsum DESC ;"
+psql -c "CREATE TABLE auto_lang_mblog AS SELECT DISTINCT ON (tweet_id) tweet_id,counting_mblog.lang, probsum as scoreMB from counting_mblog, task1, microblog WHERE task1.id_original=microblog.id_original AND microblog.id=counting_mblog.tweet_id ORDER BY tweet_id ASC, probsum DESC ;"
+psql -c "CREATE TABLE auto_lang_wiki AS SELECT DISTINCT ON (tweet_id) tweet_id,counting_wiki.lang,probsum as scorewiki from counting_wiki, task1, microblog WHERE task1.id_original=microblog.id_original AND microblog.id=counting_wiki.tweet_id ORDER BY tweet_id ASC, probsum DESC ;"
+# Display  recognition statistics
 psql -c "SELECT count(*)*100/1098.0 as all_identical FROM auto_lang_mblog, auto_lang_wiki, microblog WHERE auto_lang_mblog.tweet_id=auto_lang_wiki.tweet_id AND auto_lang_wiki.tweet_id=microblog.id AND auto_lang_wiki.lang=auto_lang_mblog.lang AND auto_lang_mblog.lang=microblog.lang; "
 psql -c "SELECT count(*)*100/1098.0 as mblog_identical FROM auto_lang_mblog, microblog WHERE auto_lang_mblog.tweet_id=microblog.id AND auto_lang_mblog.lang=microblog.lang; "
 psql -c "SELECT count(*)*100/1098.0 as wiki_identical FROM  auto_lang_wiki, microblog WHERE auto_lang_wiki.tweet_id=microblog.id AND auto_lang_wiki.lang=microblog.lang; "
 psql -c "SELECT microblog.id_original, microblog.lang as locale, auto_lang_mblog.lang as mblog, scoreMB, auto_lang_wiki.lang as wiki, scoreWiki, content FROM auto_lang_mblog, auto_lang_wiki, microblog, task1 WHERE task1.id_original=microblog.id_original AND auto_lang_mblog.tweet_id=auto_lang_wiki.tweet_id AND auto_lang_wiki.tweet_id=microblog.id AND (auto_lang_wiki.lang!=auto_lang_mblog.lang OR auto_lang_mblog.lang!=microblog.lang OR auto_lang_wiki.lang!=microblog.lang) LIMIT 10; "
-
+exit
 # How many correct identifications out of 100,000 tweets :
 select count(*)*100/100000||'%' AS correct FROM (SELECT * FROM microblog where id<=100000) AS x, auto_lang WHERE tweet_id<=100000 AND id=tweet_id AND x.lang=auto_lang.lang;
 # How many correct identifications out of 100,000 tweets (for each language) :
