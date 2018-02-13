b493c318a85bfe06183414b19d58913b8c04baa8
Dr. Pierre Jourlin <pierre.jourlin@univ-avignon.fr>
2018-02-13 15:17:11 +0100

    +wikipedia.sql
    The PostgreSQL database for Mediawiki (and xml2sql import)

---
+++ mediawiki.sql
@@ -0,0 +1,171 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.7
+-- Dumped by pg_dump version 9.6.7
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE frwiki; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON DATABASE frwiki IS 'Extrait de :
+frwiki-20180201-pages-articles-multistream.xml
+avec xml2sql 
+https://github.com/matthewfl/mediawiki-xml2sql';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: page; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE page (
+    page_id integer NOT NULL,
+    page_namespace integer NOT NULL,
+    page_title character varying(255),
+    page_restrictions pg_catalog.text,
+    page_counter bigint,
+    page_is_redirect smallint,
+    page_is_new smallint,
+    page_random real,
+    page_touched character(14),
+    page_latest integer,
+    page_len integer
+);
+
+
+--
+-- Name: TABLE page; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE page IS 'import:
+COPY page FROM ''/home/jourlin/Recherche/FELTS/data/page.txt'' ;
+
+pour mediawiki 1.5 à 1.9
++-------------------+---------------------+------+-----+---------+----------------+
+| Field             | Type                | Null | Key | Default | Extra          |
++-------------------+---------------------+------+-----+---------+----------------+
+| page_id           | int(8) unsigned     | NO   | PRI | NULL    | AUTO_INCREMENT |
+| page_namespace    | int(11)             | NO   | MUL | NULL    |                |
+| page_title        | varchar(255) binary | NO   |     | NULL    |                |
+| page_restrictions | tinyblob            | NO   |     | NULL    |                |
+| page_counter      | bigint(20) unsigned | NO   |     | 0       |                |
+| page_is_redirect  | tinyint(1) unsigned | NO   |     | 0       |                |
+| page_is_new       | tinyint(1) unsigned | NO   |     | 0       |                |
+| page_random       | real unsigned       | NO   | MUL | NULL    |                |
+| page_touched      | char(14) binary     | NO   |     | NULL    |                |
+| page_latest       | int(8) unsigned     | NO   |     | NULL    |                |
+| page_len          | int(8) unsigned     | NO   | MUL | NULL    |                |
++-------------------+---------------------+------+-----+---------+----------------+
+
+Indices';
+
+
+--
+-- Name: revision; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE revision (
+    rev_id integer NOT NULL,
+    rev_page integer,
+    rev_text_id integer,
+a    rev_comment pg_catalog.text,
+    rev_user integer,
+    rev_user_text character varying(255),
+    rev_timestamp character(14),
+    rev_minor_edit smallint,
+    rev_deleted smallint
+);
+
+
+--
+-- Name: TABLE revision; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE revision IS 'import with 
+COPY revision FROM ''/home/jourlin/Recherche/FELTS/data/revision.txt'' ;';
+
+
+--
+-- Name: text; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE text (
+    old_id integer NOT NULL,
+    old_text pg_catalog.text,
+    old_flags pg_catalog.text
+);
+
+
+--
+-- Name: TABLE text; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE text IS 'import with :
+COPY text FROM ''/home/jourlin/Recherche/FELTS/data/text.txt'' ;
+MediaWiki versions: 	1.5 – 1.9
++-----------+-----------------+------+-----+---------+----------------+
+| Field     | Type            | Null | Key | Default | Extra          |
++-----------+-----------------+------+-----+---------+----------------+
+| old_id    | int(8) unsigned | NO   | PRI | NULL    | AUTO_INCREMENT |
+| old_text  | mediumblob      | NO   |     | NULL    |                |
+| old_flags | tinyblob        | NO   |     | NULL    |                |
++-----------+-----------------+------+-----+---------+----------------+';
+
+
+--
+-- Name: page page_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY page
+    ADD CONSTRAINT page_pkey PRIMARY KEY (page_id);
+
+
+--
+-- Name: revision revision_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY revision
+    ADD CONSTRAINT revision_pkey PRIMARY KEY (rev_id);
+
+
+--
+-- Name: text text_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY text
+    ADD CONSTRAINT text_pkey PRIMARY KEY (old_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
